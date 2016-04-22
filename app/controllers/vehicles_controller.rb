f936e616846e29b76abefe3f7e8c1e180b7d14eb
Rick Barrette <rickbarrette@gmail.com>
2016-04-22 09:32:25 -0400

    Update vehicles_controller.rb

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -36,6 +36,7 @@ class VehiclesController < ApplicationController
   # display a specific vehicle
   def show
     @vehicle = Vehicle.find_by_id(params[:id])
+    @customer = @vehicle.qbo_customer.id if @vehicle.qbo_customer
   end
   
   # return an HTML form for editing a vehicle
