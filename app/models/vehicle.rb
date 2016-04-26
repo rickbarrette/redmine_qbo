4816229c5e4c89296bac00b4c3157ec3c53992a1
Rick Barrette <rickbarrette@gmail.com>
2016-04-26 06:46:46 -0400

    Update vehicle.rb

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -26,8 +26,8 @@ class Vehicle < ActiveRecord::Base
   
   def decode_vin
     if self.vin?
-      decoder = JSON.parse Edmunds::Vin.new('2dheutzvhxs28dzukx5tgu47')
-      vehicle = decoder.full(self.vin)
+      decoder = Edmunds::Vin.new('2dheutzvhxs28dzukx5tgu47')
+      vehicle = JSON.parse decoder.full(self.vin)
       self.year = vehicle['years'][0]['year']
       self.make = vehicle['make']['name']
       self.model = vehicle['model']['name']
