5f2c6d2c2013ca54013c03d7fb393f1cfec0c1f2
Rick Barrette <rickbarrette@gmail.com>
2016-04-28 12:00:04 -0400

    Update issue_patch.rb

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -22,7 +22,7 @@ module IssuePatch
     # Same as typing in the class 
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      belongs_to :qbo_customer, primary_key: :id
+      belongs_to :customer, primary_key: :id
       belongs_to :qbo_item, primary_key: :id
       belongs_to :qbo_estimate, primary_key: :id
       belongs_to :qbo_invoice, primary_key: :id
