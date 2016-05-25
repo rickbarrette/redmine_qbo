b6504333c0d6cd3dc34adf189ee70c5f7f1c2d26
Rick Barrette <rickbarrette@gmail.com>
2016-05-25 09:14:28 -0400

    Update qbo_controller.rb

--- app/controllers/qbo_controller.rb
+++ app/controllers/qbo_controller.rb
@@ -67,10 +67,10 @@ class QboController < ApplicationController
   def sync
     if Qbo.exists?
       Customer.sync
-      #QboItem.sync
-      #QboEmployee.sync
-      #QboEstimate.sync
-      #QboInvoice.sync
+      QboItem.sync
+      QboEmployee.sync
+      QboEstimate.sync
+      QboInvoice.sync
       #QboPurchase.sync
       
       # Record the last sync time
