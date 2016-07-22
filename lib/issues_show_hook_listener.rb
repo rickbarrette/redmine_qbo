8149f5ab9bf3fec65b1155ed8fc1f7e4d62febfd
Rick Barrette <rickbarrette@gmail.com>
2016-07-22 15:59:49 -0400

    Update issues_show_hook_listener.rb

--- lib/issues_show_hook_listener.rb
+++ lib/issues_show_hook_listener.rb
@@ -82,7 +82,7 @@ class IssuesShowHookListener < Redmine::Hook::ViewListener
         
       <div class=\"vehicle_vin attribute\">
         <div class=\"label\"><span>VIN</span>:</div>
-        <div class=\"value\">#{vin}</div>
+        <div class=\"value\">#{vin.gsub(/(.{9})/, '\1 ')}</div>
       </div>
       
       <div class=\"vehicle_notes attribute\">
