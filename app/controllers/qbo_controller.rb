2558cb69b16e7414da6062813e30d4fe298279d3
Rick Barrette <rickbarrette@gmail.com>
2016-07-28 19:52:42 -0400

    Update qbo_controller.rb

--- app/controllers/qbo_controller.rb
+++ app/controllers/qbo_controller.rb
@@ -32,7 +32,7 @@ class QboController < ApplicationController
   # Called when the user requests that Redmine to connect to QBO
   #
   def authenticate
-    callback = quickbooks_oauth_callback_url
+    callback = qbo_oauth_callback_url
     token = Qbo.get_oauth_consumer.get_request_token(:oauth_callback => callback)
     #session[:qb_request_token] = token
     session[:qb_request_token] = Marshal.dump(token)
