ca6e51911b8c4defc78d8458b512a97a3307a578
Rick Barrette <rickbarrette@gmail.com>
2016-07-28 08:41:23 -0400

    Webhook

--- app/controllers/qbo_controller.rb
+++ app/controllers/qbo_controller.rb
@@ -14,6 +14,7 @@ class QboController < ApplicationController
   include AuthHelper
   
   before_filter :require_user, :except => :qbo_webhook
+  skip_before_filter :verify_authenticity_token, :check_if_login_required
 
   #
   # Called when the QBO Top Menu us shown
