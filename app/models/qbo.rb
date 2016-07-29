0f8fbfb8dfb44ef25a086d08e0d31cb5f2e12433
Rick Barrette <rickbarrette@gmail.com>
2016-07-28 20:34:31 -0400

    Update qbo.rb

--- app/models/qbo.rb
+++ app/models/qbo.rb
@@ -15,7 +15,7 @@ class Qbo < ActiveRecord::Base
   OAUTH_CONSUMER_KEY = Setting.plugin_redmine_qbo['settingsOAuthConsumerKey']
   OAUTH_CONSUMER_SECRET = Setting.plugin_redmine_qbo['settingsOAuthConsumerSecret']
   
-  ::QB_OAUTH_CONSUMER = OAuth::Consumer.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, {
+  $qb_oauth_consumer = OAuth::Consumer.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, {
       :site                 => "https://oauth.intuit.com",
       :request_token_path   => "/oauth/v1/get_request_token",
       :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
@@ -31,7 +31,7 @@ class Qbo < ActiveRecord::Base
    
   def self.get_oauth_consumer
     # Quickbooks Config Info
-    return QB_OAUTH_CONSUMER
+    return $qb_oauth_consumer
   end
 
   # Get a quickbooks base object for type
