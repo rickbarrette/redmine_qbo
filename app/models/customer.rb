4d7bc59bd372af584353239d08eeed496f4d706b
Rick Barrette <rickbarrette@gmail.com>
2016-08-01 16:12:08 -0400

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -61,7 +61,7 @@ class Customer < ActiveRecord::Base
   def primary_phone=(n)
     pull unless @details
     pn = Quickbooks::Model::TelephoneNumber.new
-    pn.free_form_number = n
+    pn.free_form_number = number_to_phone(n, area_code: true)
     @details.primary_phone = pn
   end
   
@@ -81,7 +81,7 @@ class Customer < ActiveRecord::Base
   def mobile_phone=(n)
     pull unless @details
     pn = Quickbooks::Model::TelephoneNumber.new
-    pn.free_form_number = n
+    pn.free_form_number = number_to_phone(n, area_code: true)
     @details.mobile_phone = pn
   end
   
