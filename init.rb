d6dda2cdd602a03ea0c4d9d4acf3057f84e5fb9c
Rick Barrette <rickbarrette@gmail.com>
2016-08-03 23:54:17 -0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -47,7 +47,7 @@ Redmine::Plugin.register :redmine_qbo do
 
     # Register QBO top menu item
     menu :top_menu, :qbo, { :controller => :qbo, :action => :index }, :caption => 'Quickbooks', :if => Proc.new { User.current.admin? }
-    menu :top_menu, :vehicles, { :controller => :vehicles, :action => :index }, :caption => 'Vehicles', :if => Proc.new { User.current.logged? }
+    #menu :top_menu, :vehicles, { :controller => :vehicles, :action => :index }, :caption => 'Vehicles', :if => Proc.new { User.current.logged? }
     menu :top_menu, :customers, { :controller => :customers, :action => :index }, :caption => 'Customers', :if => Proc.new { User.current.logged? }
     menu :application_menu, :new_customer, { :controller => :customers, :action => :new }, :caption => 'New Customer', :if => Proc.new { User.current.logged? }
     
