d34e6cb0fd0cad10daf84e587c87b9278dfe5387
Rick Barrette <rickbarrette@gmail.com>
2016-08-03 23:05:19 -0400

    Update issues_form_hook_listener.rb

--- lib/issues_form_hook_listener.rb
+++ lib/issues_form_hook_listener.rb
@@ -46,7 +46,7 @@ class IssuesFormHookListener < Redmine::Hook::ViewListener
       vehicles = Vehicle.all.order(:name).pluck(:name, :id)
     end
     
-    vehicle = f.select :vehicles_id, update_vehicles_path(:customer_id => selected_customer), remote: true, :selected => selected_vehicle, include_blank: true
+    vehicle = f.select :vehicles_id, vehicles, :selected => selected_vehicle, include_blank: true
     
     return "<p>#{select_customer}</p> <p>#{select_item}</p> <p>#{select_invoice}</p> <p>#{select_estimate}</p> <p>#{vehicle}</p>"
   end
