2bc8ec4f56752c9556901dbdd807371b3a6e47b0
Rick Barrette <rickbarrette@gmail.com>
2016-08-05 20:25:34 -0400

    Added payments

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,34 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
-#
+# Main Quickbooks landing page
 get 'qbo', :to=> 'qbo#index'
+
+#authentication
 get 'qbo/authenticate', :to => 'qbo#authenticate'
 get 'qbo/oauth_callback', :to => 'qbo#oauth_callback'
+
+#manual sync
 get 'qbo/sync', :to => 'qbo#sync'
+
+# Estimate & Invoice PDF
 get 'qbo/estimate/:id', :to => 'estimate#show', as: :estimate
 get 'qbo/invoice/:id', :to => 'invoice#show', as: :invoice
 
+#payments
+get 'qbo/payment', :to => 'payment#new'
+post 'qbo/payment', :to => 'payment#create'
+
+#webhook
 post 'qbo/webhook', :to => 'qbo#qbo_webhook'
 
 #ajax
 get "update_vehicles" => 'vehicles#update_vehicles', as: 'update_vehicles'
 
+# Nest Vehicles under customers
 resources :customers do
   resources :vehicles
 end
 
+#allow for just vehicles too
 resources :vehicles
