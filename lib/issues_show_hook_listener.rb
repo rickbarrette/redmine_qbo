8e6e543c5b8770b4050a30c6d62777d91fa22cee
Rick Barrette <rickbarrette@gmail.com>
2016-09-01 01:32:22 -0400

    Update issues_show_hook_listener.rb

--- lib/issues_show_hook_listener.rb
+++ lib/issues_show_hook_listener.rb
@@ -88,7 +88,7 @@ class IssuesShowHookListener < Redmine::Hook::ViewListener
     </div>    "
   end
   
-  def view_issues_sidebar_issues_bottom
+  def view_issues_sidebar_issues_bottom (context={})
     issue = context[:issue]
     return "#{button_to "Bill Time", "#{Redmine::Utils::relative_url_root}/qbo/bill/#{issue.id}", method: :get}"
   end
