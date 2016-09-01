d461570b14039accaf68f2203895f437501784de
Rick Barrette <rickbarrette@gmail.com>
2016-08-31 23:44:02 -0400

    Update issues_show_hook_listener.rb

--- lib/issues_show_hook_listener.rb
+++ lib/issues_show_hook_listener.rb
@@ -33,10 +33,10 @@ class IssuesShowHookListener < Redmine::Hook::ViewListener
     
     # Invoice Number
     if issue.qbo_invoices
-      issue.qbo_invoices.each |i|
+      issue.qbo_invoices.each |i| {
         invoice =  issue.qbo_invoice.doc_number
         invoice_link = invoice_link + link_to( invoice, "#{Redmine::Utils::relative_url_root}/qbo/invoice/#{issue.qbo_invoice.id}", :target => "_blank").to_s
-      end
+      }
     end
     
     begin
