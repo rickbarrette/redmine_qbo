11d3a2d0bf194ac244790b8e1f73dc5fbf412de2
Rick Barrette <rickbarrette@gmail.com>
2016-09-01 23:28:34 -0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -47,9 +47,8 @@ Redmine::Plugin.register :redmine_qbo do
     # We are playing in the sandbox 
     #Quickbooks.sandbox_mode = true
     
-    config.gem "active_url", :source => "http://gemcutter.org"
     OAUTH_CONSUMER_SECRET = Setting.plugin_redmine_qbo['settingsOAuthConsumerSecret']
-    #ActiveUrl::Config.secret = OAUTH_CONSUMER_SECRET
+    ActiveUrl::Config.secret = OAUTH_CONSUMER_SECRET
     
     # set per_page globally
     WillPaginate.per_page = 10
