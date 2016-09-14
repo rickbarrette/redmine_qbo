8126671df9e7eef8e89e83a905b18f10ed103ba1
Rick Barrette <rickbarrette@gmail.com>
2016-09-14 08:54:29 -0400

    Update vehicles.coffee.js

--- assets/javascripts/vehicles.coffee.js
+++ assets/javascripts/vehicles.coffee.js
@@ -1,11 +1,12 @@
-$(document).on 'change', '#issue_customer_id', (evt) ->
-  $.ajax 'update_vehicles',
-    type: 'GET'
-    dataType: 'script'
-    data: {
-      customer_id: $("#issue_customer_id option:selected").val()
-    }
-    error: (jqXHR, textStatus, errorThrown) ->
-      console.log("AJAX Error: #{textStatus}")
-    success: (data, textStatus, jqXHR) ->
-      console.log("Dynamic country select OK!")
+$ ->
+  $(document).on 'change', '#issue_customer_id', (evt) ->
+    $.ajax 'update_vehicles',
+      type: 'GET'
+      dataType: 'script'
+      data: {
+        customer_id: $("#issue_customer_id option:selected").val()
+      }
+      error: (jqXHR, textStatus, errorThrown) ->
+        console.log("AJAX Error: #{textStatus}")
+      success: (data, textStatus, jqXHR) ->
+        console.log("Dynamic country select OK!")
