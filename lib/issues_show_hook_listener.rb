e542f098a87b1d8b44bf049700875a5f77ae4771
Rick Barrette <rickbarrette@gmail.com>
2016-09-13 23:24:34 -0400

    Update issues_show_hook_listener.rb

--- lib/issues_show_hook_listener.rb
+++ lib/issues_show_hook_listener.rb
@@ -23,6 +23,8 @@ class IssuesShowHookListener < Redmine::Hook::ViewListener
     # Check to see if there is a quickbooks user attached to the issue
     if issue.customer
       customer =  link_to issue.customer.name, "#{Redmine::Utils::relative_url_root}/customers/#{issue.customer.id}"
+      
+      customer = rows.left l(:field_customer), customer, :class => 'customer'
     end
     
     # Estimate Number
