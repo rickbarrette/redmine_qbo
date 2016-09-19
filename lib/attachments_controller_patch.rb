34cd6b08dca796ca037d440207e745382870bdd4
Rick Barrette <rickbarrette@gmail.com>
2016-09-19 07:29:16 -0400

    Create attachments_controller_patch.rb

---
+++ lib/attachments_controller_patch.rb
@@ -0,0 +1,38 @@
+#The MIT License (MIT)
+#
+#Copyright (c) 2016 rick barrette
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+require_dependency 'attachments_controller'
+
+module AttachmentsControllerPatch
+
+  def self.included(base) # :nodoc:
+    base.extend(ClassMethods)
+
+    base.send(:include, InstanceMethods)
+
+    # Same as typing in the class 
+    base.class_eval do
+      unloadable # Send unloadable so it will not be unloaded in development
+      
+      skip_before_filter :check_if_login_required
+    end
+  end
+      
+  module ClassMethods
+    
+  end
+  
+  module InstanceMethods
+  
+  end
+end
+
+# Add module to AttachmentsController
+AttachmentsController.send(:include, AttachmentsControllerPatch)
