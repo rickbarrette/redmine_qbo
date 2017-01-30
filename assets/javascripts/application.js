5371b0f193d363cbcb158d407dca1c654c0e51cc
Rick Barrette <rickbarrette@gmail.com>
2017-01-29 22:40:48 -0500

    Update application.js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,3 +2,7 @@
 //= require jquery-ui-1.12.1
 //= require jquery-ui-1.12.1/autocomplete
 //= require autocomplete-rails
+
+$('#issue_customer_id').bind('railsAutocomplete.select', function(event, data){
+  console.log( data.item.id);
+});
