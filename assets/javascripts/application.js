47bebf0a1ae19d4d1469619831c4ec0f3ecea0da
Rick Barrette <rickbarrette@gmail.com>
2017-02-14 14:41:34 -0500

    Delete application.js

--- assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require jquery-ui-1.12.1
-//= require jquery-ui-1.12.1/autocomplete
-//= require autocomplete-rails
-
-$('#issue_customer_id').bind('railsAutocomplete.select', function(event, data){
-  console.log( data.item.id);
-});
