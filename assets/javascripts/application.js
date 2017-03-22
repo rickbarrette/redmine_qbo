504b9b93e4708038dcdcb3ea473b8831584d59d9
Rick Barrette <rickbarrette@gmail.com>
2017-03-22 13:11:19 -0400

    Removed  {

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,6 @@
 $(function() {
     $("input#issue_customer").on("change", function() {
-        $.ajax({
+        $.ajax(
             url:  "/filter_vehicles_by_customer",
             type: "GET",
             data: { selected_customer: $("input#issue_customer").val() }
