545960e676a468aaea25b82c15748221a6eb05c1
Rick Barrette <rickbarrette@gmail.com>
2017-03-22 18:28:53 -0400

    Call autocomplete instead

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,6 @@
 $(function() {
-    $("input#issue_customer").on("change", function() {
+    $("input#issue_customer").autocomplete({
+      source: function (request, response) {
         $.ajax({
             url:  "/filter_vehicles_by_customer",
             type: "GET",
