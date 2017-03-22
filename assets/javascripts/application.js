fed22822128c69e17fab692406640f6ee867e5b4
Rick Barrette <rickbarrette@gmail.com>
2017-03-22 18:37:06 -0400

    Added debug

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,6 @@
 $(function() {
     $("input#issue_customer").autocomplete({
+      console.log("input#issue_customer.autocomplete"); 
       source: function (request, response) {
         $.ajax({
             url:  "/filter_vehicles_by_customer",
