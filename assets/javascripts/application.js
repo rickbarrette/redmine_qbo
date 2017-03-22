c725c2774c5ff7c91ed8e924f03e2baa70f0546a
Rick Barrette <rickbarrette@gmail.com>
2017-03-22 18:50:21 -0400

    Update application.js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,5 @@
 $(function() {
-  $("input#issue_customer").autocomplete({
-    console.log("input#issue_customer.autocomplete"); 
+  $("input#issue_customer").autocomplete({ 
     source: function (request, response) {
       $.ajax({ url:  "/filter_vehicles_by_customer", type: "GET", data: { selected_customer: $("input#issue_customer").val() } });
     });
