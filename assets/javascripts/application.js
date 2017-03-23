48b6df0cef219a77f935adb09763ed3ef9d6fe0a
Rick Barrette <rickbarrette@gmail.com>
2017-03-22 21:34:04 -0400

    Update application.js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,11 +1,9 @@
 $(function() {
-  $("input#issue_customer").autocomplete({ 
-    source: function (request, response) {
-      $.ajax({ 
-        url:  "/filter_vehicles_by_customer", 
-        type: "GET", 
-        data: { selected_customer: $("input#issue_customer").val() } 
-      });
-    }
+  $("input#issue_customer").on("change", function() {
+    $.ajax({
+      url:  "/filter_vehicles_by_customer",
+      type: "GET",
+      data: { selected_customer: $("input#issue_customer").val() }
+    });
   });
 });
