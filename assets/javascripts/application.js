b8186e4b52b25de096ce9c856f6f3f157fe2aab3
Rick Barrette <rickbarrette@gmail.com>
2017-11-17 10:51:25 -0500

    More customer/project relations

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -12,11 +12,12 @@ $(function() {
       data: { selected_customer: $("input#issue_customer_id").val() }
     });
     
-    $("input#project_customers_id").on("change", function() {
-    $.ajax({
-      url:  "/filter_vehicles_by_customer",
-      type: "GET",
-      data: { selected_customer: $("input#project_customers_id").val() }
-    });
+    $("input#project_customer_id").on("change", function() {
+      $.ajax({
+        url:  "/filter_vehicles_by_customer",
+        type: "GET",
+        data: { selected_customer: $("input#project_customer_id").val() }
+      });
+    });	    
   });
 });
