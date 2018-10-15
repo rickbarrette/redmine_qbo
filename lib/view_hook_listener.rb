6acc7db91b68ac7d8aba49a1ebddb052605f7181
Rick Barrette <rickbarrette@gmail.com>
2018-10-15 19:34:51 -0400

    Changed Customer search to shown on all pages

--- lib/view_hook_listener.rb
+++ lib/view_hook_listener.rb
@@ -1,3 +1,3 @@
 class ViewHookListener < Redmine::Hook::ViewListener
-  render_on :view_issues_sidebar_issues_bottom, :partial => "customers/sidebar"
+  render_on :view_layouts_base_sidebar, :partial => "customers/sidebar"
 end
