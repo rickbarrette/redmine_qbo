cc0ffce892e053c57d039fa41031feebeb44ce5d
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 15:29:18 -0500

    Oauth2 Upgrade

--- app/models/qbo_purchase.rb
+++ app/models/qbo_purchase.rb
@@ -1,6 +1,6 @@
 #The MIT License (MIT)
 #
-#Copyright (c) 2017 rick barrette
+#Copyright (c) 2020 rick barrette
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -20,11 +20,11 @@ class QboPurchase < ActiveRecord::Base
   end
 
   def get_purchase(id)
-    get_base.service.find_by_id(id)
+    get_base.find_by_id(id)
   end
 
   def self.sync
-   QboPurchase.get_base.service.all.each { |purchase|
+   QboPurchase.get_base.all.each { |purchase|
       
       purchase.line_items.all? { |line_item|
         
