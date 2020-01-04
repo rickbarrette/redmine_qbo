06e6295c6eebb53532f3c6184b8dfcaca69ad808
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 14:32:32 -0500

    Update qbo.rb

--- app/models/qbo.rb
+++ app/models/qbo.rb
@@ -34,19 +34,16 @@ class Qbo < ActiveRecord::Base
   def self.get_base(type)
     oauth2_client = get_client
     qbo = self.first
-    #access_token = OAuth2::AccessToken.new(oauth2_client, qbo.qb_token , refresh_token: qbo.qb_secret)
+    access_token = OAuth2::AccessToken.new(oauth2_client, qbo.qb_token , refresh_token: qbo.qb_secret)
     
-    # get our access token ready
-    if qbo.expire.to_date.past?
-      access_token = OAuth2::AccessToken.new(oauth2_client, qbo.qb_token , refresh_token: qbo.qb_secret)
-      new_access_token_object = access_token.refresh!
-      qbo.token = new_access_token_object
-      qbo.expire = 1.hour.from_now.utc
-      qbo.save!
-      access_token = new_access_token_object
-    else
-      access_token = qbo.token
-    end
+    # check to see if we need to refest the token
+    #if qbo.expire.to_date.past?
+    #  new_access_token_object = access_token.refresh!
+    #  qbo.token = new_access_token_object
+    #  qbo.expire = 1.hour.from_now.utc
+    #  qbo.save!
+    #  access_token = new_access_token_object
+    #end
     
     case type
       when :item
