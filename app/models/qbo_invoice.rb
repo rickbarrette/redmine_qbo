7c42197cb1c1505f4cb2bb66a6d1f7fd27bfa14a
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 15:30:03 -0500

    Oauth2 Upgrade

--- app/models/qbo_invoice.rb
+++ app/models/qbo_invoice.rb
@@ -1,6 +1,6 @@
 #The MIT License (MIT)
 #
-#Copyright (c) 2017 rick barrette
+#Copyright (c) 2020 rick barrette
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -18,7 +18,7 @@ class QboInvoice < ActiveRecord::Base
   self.primary_key = :id
   
   def self.get_base
-    Qbo.get_base(:invoice).service
+    Qbo.get_base(:invoice)
   end
   
   # sync ALL the invoices
