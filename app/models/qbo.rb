19a6180e155aafc48b6c2ec8117573fb2a6eb73f
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 12:26:04 -0500

    Update qbo.rb

--- app/models/qbo.rb
+++ app/models/qbo.rb
@@ -23,6 +23,11 @@ class Qbo < ActiveRecord::Base
   oauth2_client = OAuth2::Client.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, oauth_params)
   
   def self.get_client
+    oauth_params = {
+    site: "https://appcenter.intuit.com/connect/oauth2",
+    authorize_url: "https://appcenter.intuit.com/connect/oauth2",
+    token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+  }
     return OAuth2::Client.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, oauth_params)
   end
     
