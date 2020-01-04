a4111e0a1188e4d19fb5d91cbe81f2ac00935697
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 15:36:36 -0500

    oAuth2 Upgrade

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,6 @@
 #The MIT License (MIT)
 #
-#Copyright (c) 2017 rick barrette
+#Copyright (c) 2020 rick barrette
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -23,7 +23,7 @@ class Payment
     payment.deposit_to_account_id = @account_id.to_i
     payment.payment_method_id = @payment_method_id.to_i
     payment.total = @total_amount
-    Qbo.get_base(:payment).service.update(payment)
+    Qbo.get_base(:payment).update(payment)
   end
   
   def save!
