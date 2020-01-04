0fd2abbec38915d7c2858262d3821fe205c67c9c
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 15:28:43 -0500

    Oauth2 Upgrade

--- app/models/qbo_item.rb
+++ app/models/qbo_item.rb
@@ -1,6 +1,6 @@
 #The MIT License (MIT)
 #
-#Copyright (c) 2017 rick barrette
+#Copyright (c) 2020 rick barrette
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -27,9 +27,9 @@ class QboItem < ActiveRecord::Base
     query << " AND  Metadata.LastUpdatedTime >= '#{last.iso8601}' " if last 
    
     if count == 0
-      items = get_base.service.all
+      items = get_base.all
     else
-      items = get_base.service.query(query)
+      items = get_base.query(query)
     end
 
     unless items.count = 0
