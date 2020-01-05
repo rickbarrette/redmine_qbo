18ada91fcdab4de42c694ecb392515bf62d5faa2
Rick Barrette <rickbarrette@gmail.com>
2020-01-04 23:38:46 -0500

    Update qbo.rb

--- app/models/qbo.rb
+++ app/models/qbo.rb
@@ -35,7 +35,7 @@ class Qbo < ActiveRecord::Base
     oauth2_client = get_client
     qbo = self.first
     #access_token = OAuth2::AccessToken.new(oauth2_client, qbo.qb_token , refresh_token: qbo.qb_secret)
-    access_token = OAuth2::AccessToken.from_hash(oauth2_client, Hash.try_convert( qbo.token) )
+    access_token = OAuth2::AccessToken.from_hash(oauth2_client, eval( qbo.token) )
 
     # check to see if we need to refresh the token
     if qbo.expire.to_date.past?
