f66fbf66569b6542db1069a94a093c81eb981ee7
Rick Barrette <rickbarrette@gmail.com>
2022-02-12 16:14:36 -0500

    specified the rails version in migrations

--- db/migrate/026_create_line_items.rb
+++ db/migrate/026_create_line_items.rb
@@ -1,6 +1,6 @@
 #The License
 #
-#Copyright (c) 2018 Rick Barrette - All Rights Reserved
+#Copyright (c) 2022 Rick Barrette - All Rights Reserved
 # 
 #Unauthorized copying of this software and associated documentation files (the "Software"), via any medium is strictly prohibited.
 #
@@ -10,7 +10,7 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-class CreateLineItems < ActiveRecord::Migration
+class CreateLineItems < ActiveRecord::Migration[5.1]
   def change  
     create_table :line_items do |t| 
       t.integer  :item_id
