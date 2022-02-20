8c735d39218611cf6bd32dbb37f6ec16b0432595
Ricky Barrette <rickbarrette@gmail.com>
2022-02-20 17:19:56 -0500

    Added Estimate date

--- app/models/qbo_estimate.rb
+++ app/models/qbo_estimate.rb
@@ -62,5 +62,35 @@ class QboEstimate < ActiveRecord::Base
     estimate = base.fetch_by_id(id)
     return base.pdf(estimate)
   end
+
+  # Magic Method
+  # Maps Get/Set methods to QBO estimate object
+  def method_missing(sym, *arguments)  
+    # Check to see if the method exists
+    if Quickbooks::Model::Estimate.method_defined?(sym)
+      # download details if required
+      pull unless @details
+      method_name = sym.to_s
+      # Setter
+      if method_name[-1, 1] == "="  
+        @details.method(method_name).call(arguments[0])  
+      # Getter
+      else  
+        return @details.method(method_name).call 
+      end
+    end  
+  end
+
+  private
+  
+  # pull the details
+  def pull
+    begin
+      raise Exception unless self.id
+      @details = Qbo.get_base(:estimate).fetch_by_id(self.id)
+    rescue Exception => e
+      @details = Quickbooks::Model::Estimate.new
+    end
+  end
   
 end
