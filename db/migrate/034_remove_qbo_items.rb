3159289ac03ec1eb64e8a014991007ffeedf249a
Ricky Barrette <rickbarrette@gmail.com>
2022-03-06 17:26:57 -0500

    Removed unused code, only need to bill time
    removed line items, payments, drop used db tables

--- db/migrate/034_remove_qbo_items.rb
+++ db/migrate/034_remove_qbo_items.rb
@@ -8,40 +8,10 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-class QboItem < ActiveRecord::Base
-  unloadable
-  has_many :issues
-  #attr_accessible :name
-  validates_presence_of :id, :name
-
-  self.primary_key = :id
-
-  def self.get_base
-    Qbo.get_base(:item)
+class RemoveQboItems < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :qbo_items
+    drop_table :qbo_purchases
+    drop_table :line_items
   end
-  
-  def self.sync 
-    last = Qbo.first.last_sync
-
-    query = "SELECT Id, Name FROM Item WHERE Type = 'Service' "
-    query << " AND  Metadata.LastUpdatedTime >= '#{last.iso8601}' " if last 
-   
-    if count == 0
-      items = get_base.all
-    else
-      items = get_base.query(query)
-    end
-
-    unless items.count = 0
-      items.find_by(:type, "Service").each { |i|
-        qbo_item = QboItem.find_or_create_by(id: i.id)
-        qbo_item.name = i.name
-        qbo_item.id = i.id
-        qbo_item.save
-      }
-    end
-      
-#    QboItem.where.not(items.map(&:id)).destroy_all
-  end
-  
 end
