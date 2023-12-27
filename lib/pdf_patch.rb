b3a809ab1cd445a3750d104a4ac7f496574b6a22
Ricky Barrette <rickbarrette@gmail.com>
2023-12-27 15:03:08 -0500

    Redmine 5.1 Update

--- lib/pdf_patch.rb
+++ lib/pdf_patch.rb
@@ -1,6 +1,6 @@
 #The MIT License (MIT)
 #
-#Copyright (c) 2022 rick barrette
+#Copyright (c) 2023 rick barrette
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -11,7 +11,7 @@
 require_dependency 'redmine/export/pdf'
 require_dependency 'redmine/export/pdf/issues_pdf_helper'
 
-module IssuesPdfHelperPatch
+module PdfPatch
 
   def self.included(base)
     base.send(:include, InstanceMethods)
@@ -256,4 +256,4 @@ module IssuesPdfHelperPatch
   end
 end
 
-Redmine::Export::PDF::IssuesPdfHelper.send(:include, IssuesPdfHelperPatch)
+Redmine::Export::PDF::IssuesPdfHelper.send(:include, PdfPatch)
