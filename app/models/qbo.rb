f6da031e72c71699636c2467c88bedeaa3327239
Ricky Barrette <rickbarrette@gmail.com>
2025-06-15 18:59:00 -0400

    include Redmine::I18n

--- app/models/qbo.rb
+++ app/models/qbo.rb
@@ -1,6 +1,6 @@
 #The MIT License (MIT)
 #
-#Copyright (c) 2024 rick barrette
+#Copyright (c) 2016 - 2025 rick barrette
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -11,6 +11,7 @@
 class Qbo < ActiveRecord::Base
     
   include QuickbooksOauth
+  include Redmine::I18n
   
   # Updates last sync time stamp
   def self.update_time_stamp
