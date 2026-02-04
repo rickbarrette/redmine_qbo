c36f4c905bdaad244371b9f8d482d2d25f640304
Rick Barrette <rickbarrette@gmail.com>
2026-02-03 22:42:08 -0500

    moved issue hooks into issue_patch

--- lib/patches/issue_patch.rb
+++ lib/patches/issue_patch.rb
@@ -12,9 +12,9 @@ require_dependency 'issue'
 
 module Patches
 
-
   # Patches Redmine's Issues dynamically.
-  # Adds a relationships
+  # Adds relationships for customers, estimates, invoices, customer_tokens
+  # Adds before and after save hooks
   module IssuePatch
 
     def self.included(base) # :nodoc:
@@ -28,6 +28,8 @@ module Patches
         belongs_to :customer_token, primary_key: :id
         belongs_to :estimate, primary_key: :id
         has_and_belongs_to_many :invoices
+        before_save :titlize_subject
+        after_save :bill_time
       end
       
     end
@@ -40,61 +42,62 @@ module Patches
       
       # Create billable time entries
       def bill_time
-      
-        # Check to see if we have everything we need to bill the customer
+        logger.debug "QBO: Billing time for issue ##{id} - #{subject}"
+        return unless  status.is_closed?
         return if assigned_to.nil?
         return unless Qbo.first 
         return unless customer 
 
-        # Get unbilled time entries
-        spent_time = time_entries.where(billed: [false, nil])
-        spent_hours ||= spent_time.sum(:hours) || 0
-        
-        if spent_hours > 0 then
+        Thread.new do
+          spent_time = time_entries.where(billed: [false, nil])
+          spent_hours ||= spent_time.sum(:hours) || 0
           
-          # Prepare to create a new Time Activity
-          qbo = Qbo.first
-          qbo.perform_authenticated_request do |access_token|
-            time_service = Quickbooks::Service::TimeActivity.new(company_id: qbo.realm_id, access_token: access_token)
-            item_service = Quickbooks::Service::Item.new(company_id: qbo.realm_id, access_token: access_token)
-            time_entry = Quickbooks::Model::TimeActivity.new
-    
-            # Lets total up each activity before billing.
-            # This will simpify the invoicing with a single billable time entry per time activity
-            h = Hash.new(0)
-            spent_time.each do |entry|
-              h[entry.activity.name] += entry.hours
-              # update time entries billed status
-              entry.billed = true
-              entry.save
-            end
+          if spent_hours > 0 then
             
-            # Now letes upload our totals for each activity as their own billable time entry
-            h.each do |key, val|
+            # Prepare to create a new Time Activity
+            qbo = Qbo.first
+            qbo.perform_authenticated_request do |access_token|
+              time_service = Quickbooks::Service::TimeActivity.new(company_id: qbo.realm_id, access_token: access_token)
+              item_service = Quickbooks::Service::Item.new(company_id: qbo.realm_id, access_token: access_token)
+              time_entry = Quickbooks::Model::TimeActivity.new
+      
+              # Lets total up each activity before billing.
+              # This will simpify the invoicing with a single billable time entry per time activity
+              h = Hash.new(0)
+              spent_time.each do |entry|
+                h[entry.activity.name] += entry.hours
+                # update time entries billed status
+                entry.billed = true
+                entry.save
+              end
               
-              # Convert float spent time to hours and minutes
-              hours = val.to_i
-              minutesDecimal = (( val - hours) * 60)
-              minutes = minutesDecimal.to_i
+              # Now letes upload our totals for each activity as their own billable time entry
+              h.each do |key, val|
+                
+                # Convert float spent time to hours and minutes
+                hours = val.to_i
+                minutesDecimal = (( val - hours) * 60)
+                minutes = minutesDecimal.to_i
 
-              # Lets match the activity to an qbo item
-              item = item_service.query("SELECT * FROM Item WHERE Name = '#{key}' ").first
-              next if item.nil?
-              
-              # Create the new billable time entry and upload it
-              time_entry.description = "#{tracker} ##{id}: #{subject} #{"(Partial @ #{done_ratio}%)" if not closed?}"
-              time_entry.employee_id = assigned_to.employee_id 
-              time_entry.customer_id = customer_id
-              time_entry.billable_status = "Billable"
-              time_entry.hours = hours
-              time_entry.minutes = minutes
-              time_entry.name_of = "Employee"
-              time_entry.txn_date = Date.today
-              time_entry.hourly_rate = item.unit_price
-              time_entry.item_id = item.id 
-              time_entry.start_time = start_date
-              time_entry.end_time = Time.now
-              time_service.create(time_entry)
+                # Lets match the activity to an qbo item
+                item = item_service.query("SELECT * FROM Item WHERE Name = '#{key}' ").first
+                next if item.nil?
+                
+                # Create the new billable time entry and upload it
+                time_entry.description = "#{tracker} ##{id}: #{subject} #{"(Partial @ #{done_ratio}%)" if not closed?}"
+                time_entry.employee_id = assigned_to.employee_id 
+                time_entry.customer_id = customer_id
+                time_entry.billable_status = "Billable"
+                time_entry.hours = hours
+                time_entry.minutes = minutes
+                time_entry.name_of = "Employee"
+                time_entry.txn_date = Date.today
+                time_entry.hourly_rate = item.unit_price
+                time_entry.item_id = item.id 
+                time_entry.start_time = start_date
+                time_entry.end_time = Time.now
+                time_service.create(time_entry)
+              end
             end
           end
         end
@@ -104,6 +107,11 @@ module Patches
     # Create a shareable link for a customer
     def share_token
       CustomerToken.get_token self
+    end
+    
+    # Titleize the subject before save
+    def titlize_subject
+      self.subject = self.subject.titleize
     end
     
   end  
