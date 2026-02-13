e63b9e4217be5f8560f75b9e1d50161de4a2068b
Rick Barrette <rickbarrette@gmail.com>
2026-02-13 07:32:20 -0500

    use safe_join

--- lib/redmine_qbo/hooks/view_hook_listener.rb
+++ lib/redmine_qbo/hooks/view_hook_listener.rb
@@ -15,10 +15,11 @@ module RedmineQbo
 
       # Load the javascript to support the autocomplete forms
       def view_layouts_base_html_head(context = {})
-        js = javascript_include_tag 'application.js', plugin: :redmine_qbo
-        js += javascript_include_tag 'autocomplete-rails.js', plugin: :redmine_qbo
-        js += javascript_include_tag 'checkbox_controller.js', plugin: :redmine_qbo
-        return js
+        safe_join([
+          javascript_include_tag( 'application.js', plugin: :redmine_qbo),
+          javascript_include_tag( 'autocomplete-rails.js', plugin: :redmine_qbo),
+          javascript_include_tag( 'checkbox_controller.js', plugin: :redmine_qbo)
+        ])
       end
 
       render_on :view_layouts_base_sidebar, partial: "qbo/sidebar"
