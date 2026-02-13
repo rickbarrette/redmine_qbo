1d477032069eba96de684234eb83f1787b78981a
Rick Barrette <rickbarrette@gmail.com>
2026-02-13 18:37:12 -0500

    fixed indentiation

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -9,7 +9,7 @@ function getSelectedDocs() {
   const appointent_extras = document.querySelectorAll('.appointment');
 
   let output = '';
-for (const item of appointent_extras) {
+  for (const item of appointent_extras) {
     if (item.checked) {
       console.log(`Checked item: ${item.dataset.text} with URL: ${item.dataset.url}`);
       output += `%0A`+ encodeURIComponent(`<a href="${window.location.origin}${item.dataset.url}">${item.dataset.text}</a>`) +`%0A`;
