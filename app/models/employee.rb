ea0f42b68e25e27479a120c02748f24c4a118f94
Rick Barrette <rickbarrette@gmail.com>
2026-02-27 23:18:40 -0500

    Added comment

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -13,6 +13,7 @@ class Employee < ActiveRecord::Base
   has_many :users
   validates_presence_of :id, :name
 
+  # Sync all employees, typically triggered by a scheduled task or manual sync request
   def self.sync
     EmployeeSyncJob.perform_later(full_sync: true)
   end
